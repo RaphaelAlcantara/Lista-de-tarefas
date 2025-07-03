41cb7df2adb3924d85810700d1408ec3261fd2c0
Raphael Victor Freitas Ramos de Alcântara <raphaelvictor40@gmail.com>
2025-07-03 20:11:38 -0300

    botao login
    botao login pega dados e cria a variavel

---
+++ login/script.js
@@ -0,0 +1,8 @@
+function login() {
+nome_login = document.getElementById("user_login");
+senha_login = document.getElementById("user_password");
+
+
+    console.log("User: " + nome_login.value +
+        " and password: " + senha_login.value);
+}
