6e0e7751f0b813e015bffa287f2c85cc2f4534f4
Raphael Victor Freitas Ramos de Alcântara <raphaelvictor40@gmail.com>
2025-07-03 21:18:20 -0300

    js cadastro iniciado

--- login/script.js
+++ login/script.js
@@ -1,8 +1,28 @@
+var usuarios = [];
+
 function login() {
-nome_login = document.getElementById("user_login");
-senha_login = document.getElementById("user_password");
+    nome_login = document.getElementById("user_login");
+    senha_login = document.getElementById("user_password");
 
 
     console.log("User: " + nome_login.value +
         " and password: " + senha_login.value);
 }
+
+
+function cadastrar() {
+    email_cadastro = document.getElementById("email_cadastro");
+    senha_cadastro = document.getElementById("senha_cadastro");
+
+    usuarios.push({
+        nome: email_cadastro.value,
+        senha: senha_cadastro.value
+    });
+
+    console.log("Usuário cadastrado: " + email_cadastro.value);
+    console.log(email_cadastro.value);
+    console.log(senha_cadastro.value);
+}
+
+console.log(email_cadastro.value);
+console.log(senha_cadastro.value);
