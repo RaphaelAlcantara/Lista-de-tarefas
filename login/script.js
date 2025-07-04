c09b8cbf3ca743b014eea573c1add4b34656265b
JasonKauan <jasonkauandiass@gmail.com>
2025-07-04 17:14:26 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -14,15 +14,16 @@ function login() {
   } 
 
     
-
+ 
  let usuarioEncontrado = usuarios.find(user => user.nome === nome_login && user.senha === senha_login);
   
-
+  
     if (usuarioEncontrado){
         alert(`login bem sucedido! bem vindo` );
     }else  {
         alert("Usuário ou senha incorretos.");
-    }
+    } 
+
 
     if (localStorage.getItem("usuarios")) {
     usuarios = JSON.parse(localStorage.getItem("usuarios"));
